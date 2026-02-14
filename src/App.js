195cd3f02c9243e994d87f8c8ee5df3a834cefec
bishtebisht <bhoomibisht007@gmail.com>
2026-02-14 23:16:54 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import SearchIcon from './search.svg';
 //262db138 (API key)
 
 const API_URL ='http://www.omdbapi.com?apiKey=262db138';
+
+const movie1 = {
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzA2NDQzZDEtNDU5Ni00YTlkLTg2OWEtYmQwM2Y1YTBjMjFjXkEyXkFqcGc@._V1_SX300.jpg",
+    "Title": "Batman Begins",
+    "Type": "movie",
+    "Year": "2005",
+    "imdbID": "tt0372784"
+}
 const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`); 
@@ -27,6 +35,9 @@ const App = () => {
                 src={SearchIcon} 
                 alt="search" 
                 onClick={()=>{}}/>
+            </div>
+            <div className="container">
+
             </div>
         </div>
   );
